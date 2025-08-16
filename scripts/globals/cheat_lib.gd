45972b5d0fed133bf8ecdba09157c80236772d03
meloncolle <melon@meloncolle.com>
2025-08-16 05:28:25 -0700

    Fixed checking cheat substrins

--- scripts/globals/cheat_lib.gd
+++ scripts/globals/cheat_lib.gd
@@ -3,8 +3,7 @@ extends Node
 @export var codes: Array[CheatCode]
 
 func _ready():
-	print("bubba")
-	# We want to make sure no codes in library have inputs as subsequences of other codes
+	# We want to make sure no codes in library have inputs as subsets of other codes
 	# We could comment this out and just check manually bc its kind of wasteful >_>
 	var c_string: String
 	var d_string: String
@@ -13,7 +12,7 @@ func _ready():
 		for d in codes:
 			if c == d: continue
 			d_string = inputs_to_string(d.sequence)
-			assert(!c_string.is_subsequence_of(d_string), "Cheat '%s' inputs are subsequence of cheat '%s'" % [c.name, d.name])
+			assert(c_string not in (d_string), "Cheat '%s' inputs are subsequence of cheat '%s'" % [c.name, d.name])
 
 # Check if any cheat code sequence is found in input
 # If match found, return CheatCode resource
@@ -23,11 +22,11 @@ func find_match(input: Array[Enums.CheatInput]) -> CheatCode:
 	var code_string: String
 	for c in codes:
 		code_string = inputs_to_string(c.sequence)
-		if code_string.is_subsequence_of(input_string):
+		if code_string in input_string:
 			return c
 	return null
 
-# Convert CheatInput sequence to string of numbers, so we can easily use subsequence func
+# Convert CheatInput sequence to string of numbers, so we can easily check subset
 func inputs_to_string(input: Array[Enums.CheatInput]) -> String:
 	var result := ""
 	for i in input:
